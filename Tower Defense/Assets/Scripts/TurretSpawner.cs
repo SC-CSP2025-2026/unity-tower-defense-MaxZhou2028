62ed99b771d9382c808597362d693e59030bea23
Max Zhou <Zixing.Zhou@scsstudent.org>
2026-06-03 22:36:51 -0700

    feat: completed 17.5

--- Tower Defense/Assets/Scripts/TurretSpawner.cs
+++ Tower Defense/Assets/Scripts/TurretSpawner.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TurretSpawner : MonoBehaviour
 {
@@ -33,11 +34,15 @@ public class TurretSpawner : MonoBehaviour
     {
         if (tileController.IsOccupied)
         {
-            return;
+            return;//don't spawn a turret if it's already occuiped
         }
-        GameObject newTurret = Instantiate(TurretPrefab);
-        newTurret.transform.position = tileController.transform.position;
-        tileController.IsOccupied = true;
+        if (CanSpawn(tileController))
+        {
+            GameObject newTurret = Instantiate(TurretPrefab);
+            newTurret.transform.position = tileController.transform.position;
+            tileController.IsOccupied = true;
+            Controller.Gold -= 50;
+        }
     }
 
     public void StopListeningToTilesIn(GameObject grid)
@@ -47,6 +52,40 @@ public class TurretSpawner : MonoBehaviour
             tile.OnCursorClicked.RemoveListener(SpawnTurret);
         }
     }
+
+    public bool CanSpawn(TileController tileController)
+    {
+        if (tileController.IsOccupied)
+        {
+            return false;
+        }
+
+        if(Controller.Gold <50)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void ShowInfo(TileController tileController)
+    {
+        if (tileController.IsOccupied)
+        {
+            Controller.InfoLabel.text = "Cannot build here";
+        }
+        else if (Controller.Gold <50)
+        {
+            Controller.InfoLabel.text = "<color=red>Not Enought Gold/color";
+        }
+        else
+        {
+            Controller.InfoLabel.text = "50 Gold - Place Turret";
+        }
+    }
+
+
+
+
 
 
 
